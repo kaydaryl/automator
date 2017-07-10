906d5c1dd52b08c91e5640398262f5f877494406
Daryl <kaydaryl@gmail.com>
2017-07-10 16:06:40 -0700

    update

--- 264to265.sh
+++ 264to265.sh
@@ -14,26 +14,28 @@ else
 fi
 rm -f /tmp/264to265list /RAID/tmpoutput.mp4
 echo "Looking for files in: $folderToParse"
-find "$folderToParse" -name "*.mkv" -o -name "*.mp4" -o -name "*.avi" > /tmp/264to265list
 
-touch /tmp/264to265list
+find "/RAID/tmpvideoreencode/" -type f -name "*.mkv" -o -name "*.mp4" -o -name "*.avi" > /tmp/264to265list
+touch /RAID/filesleftas264
 
 while read line; do
-    if [[ $(mediainfo "$line" | grep "library" | grep 264 | wc -l) > 0 && $(grep -rnw "$line" /RAID/filesleftas264 | wc -l) > 0 ]]; then
+    if [[ $(mediainfo "$line" | grep "HEVC" | wc -l) > 0 && $(grep -rnw "$line" /RAID/filesleftas264 | wc -l) == 0 ]]; then
 	echo "Transcoding: $(basename "$line")"
-	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "/RAID/tmpoutput.mp4"
+	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "/RAID/tmpoutput.mp4" > /dev/null 2>&1
 	newfilesize=$(du -k "/RAID/tmpoutput.mp4" | cut -f1)
 	origfilesize=$(du -k "$line" | cut -f1)
 	threshold=$(($origfilesize * 4 / 5))
 	#if newfilesize > threshold && newfilesize > 0 && $? ==0
-	if [[ $newfilesize < $threshold && $newfilesize > 0 ]]; then
-	    mv /RAID/tmpoutput.mp4 "$line"
+	if [[ $? == 0 ]]; then
+	    if [[ $newfilesize < $threshold && $newfilesize > 0 ]]; then
+		mv /RAID/tmpoutput.mp4 "$line"
+	    else
+		echo "$line" >> /RAID/filesleftas264.log
+	    fi
 	else
-	    echo "$line" >> /RAID/filesleftas264
+	    echo "$lines" >> /RAID/filesthatfailed.log
 	fi
 	echo ""
 	echo ""
     fi
-done < /tmp/264to265list
-
-rm -f /tmp/264to265list
+    done < /tmp/264to265list
