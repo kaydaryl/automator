669edf0795751ad65af1f9d3a2f31cf121ed233d
server <kaydaryl@gmail.com>
2017-07-11 16:32:56 -0700

    updates

--- 264to265.sh
+++ 264to265.sh
@@ -12,31 +12,29 @@ if [[ $1 != "" ]]; then
 else
     folderToParse="$PWD"
 fi
-rm -f /tmp/264to265list /RAID/tmpoutput.mp4
 echo "Looking for files in: $folderToParse"
 
-find "/RAID/tmpvideoreencode/" -type f -name "*.mkv" -o -name "*.mp4" -o -name "*.avi" > /tmp/264to265list
-touch /RAID/filesleftas264
+touch /RAID/filesthatfailed.log "$folderToParse/.filestoconvert.log"
 
-while read line; do
-    if [[ $(mediainfo "$line" | grep "HEVC" | wc -l) > 0 && $(grep -rnw "$line" /RAID/filesleftas264 | wc -l) == 0 ]]; then
-	echo "Transcoding: $(basename "$line")"
-	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "/RAID/tmpoutput.mp4" > /dev/null 2>&1
-	newfilesize=$(du -k "/RAID/tmpoutput.mp4" | cut -f1)
+find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" >> "$folderToParse/.filestoconvert.log"
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [[ $(mediainfo "$line" | grep "HEVC" | wc -l) < 1 ]]; then
+	echo "Transcoding: $basename"$line""
+	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
+	newfilesize=$(du -k "$folderToParse/.tmpoutput.mp4" | cut -f1)
 	origfilesize=$(du -k "$line" | cut -f1)
-	threshold=$(($origfilesize * 4 / 5))
-	#if newfilesize > threshold && newfilesize > 0 && $? ==0
 	if [[ "$?" == "0" ]]; then
-	    if [[ $newfilesize < $threshold && $newfilesize > 0 ]]; then
-		mv /RAID/tmpoutput.mp4 "$line"
-	    else
-		echo "$line" >> /RAID/filesleftas264.log
-	    fi
-	else
+            if [[ $newfilesize < $origfilesize && $newfilesize > 0 ]]; then
+                echo "Moving: $basename"$line""
+		mv "$folderToParse/.tmpoutput.mp4" "$line"
+            fi
+        else
+            $line >> /RAID/filesthatfailed.log
 	    echo "$(basename "$line") failed"
-	    echo "$lines" >> /RAID/filesthatfailed.log
 	fi
-	echo ""
-	echo ""
     fi
-    done < /tmp/264to265list
+done < "$folderToParse/.filestoconvert.log"
+
+rm -r "$folderToParse/.filestoconvert.log" "$folderToParse/.tmpoutput.mp4"
+
