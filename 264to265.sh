0306a0ab8646dfdad1e09f5a7aa78ce615d12733
root <kaydaryl@gmail.com>
2017-07-12 10:18:10 -0700

    updates

--- 264to265.sh
+++ 264to265.sh
@@ -15,20 +15,17 @@ fi
 echo "Looking for files in: $folderToParse"
 
 touch /RAID/filesthatfailed.log "$folderToParse/.filestoconvert.log"
+rm -f "$folderToParse/.tmpoutput.mp4" "$folderToParse/.filestoconvert.log"
 
 find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" >> "$folderToParse/.filestoconvert.log"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     if [[ $(mediainfo "$line" | grep "HEVC" | wc -l) < 1 ]]; then
 	echo "Transcoding: $basename"$line""
-	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
-	newfilesize=$(du -k "$folderToParse/.tmpoutput.mp4" | cut -f1)
-	origfilesize=$(du -k "$line" | cut -f1)
+	ffmpeg -y -i "$line" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 -t 1 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
 	if [[ "$?" == "0" ]]; then
-            if [[ $newfilesize < $origfilesize && $newfilesize > 0 ]]; then
-                echo "Moving: $basename"$line""
-		mv "$folderToParse/.tmpoutput.mp4" "$line"
-            fi
+	    echo "Moving: $basename"$line""
+	    #mv "$folderToParse/.tmpoutput.mp4" "$line"
         else
             $line >> /RAID/filesthatfailed.log
 	    echo "$(basename "$line") failed"
@@ -36,5 +33,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     fi
 done < "$folderToParse/.filestoconvert.log"
 
-rm -r "$folderToParse/.filestoconvert.log" "$folderToParse/.tmpoutput.mp4"
+rm -f "$folderToParse/.filestoconvert.log" "$folderToParse/.tmpoutput.mp4"
 
