b75a50efb6a47610225e36e9a36522c9854b62ae
root <kaydaryl@gmail.com>
2017-08-01 19:06:59 -0700

    fixed nice levels.

--- 264to265.sh
+++ 264to265.sh
@@ -6,18 +6,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-function displaytime {
-  local T=$1
-  local D=$((T/60/60/24))
-  local H=$((T/60/60%24))
-  local M=$((T/60%60))
-  local S=$((T%60))
-  [[ $D > 0 ]] && printf '%d days ' $D
-  [[ $H > 0 ]] && printf '%d hours ' $H
-  [[ $M > 0 ]] && printf '%d minutes ' $M
-  [[ $D > 0 || $H > 0 || $M > 0 ]] && printf 'and '
-  printf '%d seconds\n' $S
-}
 
 if [[ $1 != "" ]]; then
     folderToParse="$1"
@@ -34,26 +22,22 @@ find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -ina
 IFS=$'\n'
 for i in $(cat "$folderToParse/.filestoconvert.log"); do
     if [[ $(mediainfo "$i" | grep "HEVC" | wc -l) < 1 ]]; then
-	echo "$i" >> "$folderToParse/.264files.log"
-	counter=$[$counter + 1]
+    echo "$i" >> "$folderToParse/.264files.log"
+    counter=$[$counter + 1]
     fi
 done
 
-echo "Files to process: $counter"
+echo $counter
 
 for i in $(cat "$folderToParse/.264files.log"); do
     if [[ $(mediainfo "$i" | grep "HEVC" | wc -l) < 1 ]]; then
-	echo "Transcoding: $basename"$i""
-	SECONDS=0
-	nice -19 ffmpeg -y -i "$i" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1 &
-	sudo pv -pd $(pgrep -n "ffmpeg")
-	NEWNAME=$(echo $i | rev | cut -c 4- | rev)
-        NEWNAME+=mp4
-        echo "Moving: $NEWNAME"
-	rm "$i"
-	mv "$folderToParse/.tmpoutput.mp4" "$NEWNAME"
-	counter=$[$counter - 1]
-	echo "Time to transcode: $SECONDS"
+    echo "Transcoding: $basename"$i""
+    sudo nice -n 19 ffmpeg -y -i "$i" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
+    if [[ "$?" == "0" ]]; then
+        echo "Moving: $basename"$i""
+        mv "$folderToParse/.tmpoutput.mp4" "$i"
+        counter=$[$counter - 1]
+    fi
     echo ""
     echo ""
     fi
@@ -63,4 +47,3 @@ done
 rm -f "$folderToParse/.filestoconvert.log" "$folderToParse/.tmpoutput.mp4" "$folderToParse/.264files.log"
 
 echo "Happy streaming!"
-
