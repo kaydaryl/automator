c61808aba55fb75a4987c01f66507e7373b5e231
Daryl <kaydaryl@gmail.com>
2017-08-21 19:09:01 -0700

    updates to 264to265 to remove excess files, update display

--- 264to265.sh
+++ 264to265.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-function CLEANUP {
-   find "$folderToParse" -name ".tmpoutput.mp4" -o -name ".264files.log" -o -name ".filestoconvert.log" | while read fname; do rm "$fname"; done
+function HOWMANY {
+    COUNTER=$(find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.m4v" | wc -l)
 }
 
 set -e
@@ -11,7 +10,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-
 if [[ $1 != "" ]]; then
     folderToParse="$1"
 else
@@ -19,42 +17,40 @@ else
 fi
 echo "Looking for files in: $folderToParse"
 
-CLEANUP
 
-counter=0
-find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.m4v" | sort >> "$folderToParse/.filestoconvert.log"
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
 
 IFS=$'\n'
-for i in $(cat "$folderToParse/.filestoconvert.log"); do
-    if [[ $(mediainfo "$i" | grep "HEVC" | wc -l) < 1 ]]; then
-       echo "$i" >> "$folderToParse/.264files.log"
-       counter=$((counter + 1))
-    fi
-done
-if [[ $counter == 0 ]]; then
+if [[ $(find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.m4v" | wc -l) == 0 ]]; then
     echo "This folder is all x265!"
     exit 0
+else
+    HOWMANY
 fi
 
-echo "Total Files to be processed: $counter"
 
-for i in $(cat "$folderToParse/.264files.log"); do
+for i in $(find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.m4v" | sort -n); do
     if [[ $(mediainfo "$i" | grep "HEVC" | wc -l) < 1 ]]; then
-    	echo "Transcoding: $(basename "$i")"
-	sudo nice -n 19 ffmpeg -y -i "$i" -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
+    	echo -e "${yellow}Transcoding: $(basename "$i")${reset}"
+	sudo nice -n 19 ffmpeg -y -i "$i" -xerror -movflags faststart -c:a aac -c:v libx265 -preset medium -crf 19 "$folderToParse/.tmpoutput.mp4" > /dev/null 2>&1
     	if [[ "$?" == "0" ]]; then
 	    mv "$folderToParse/.tmpoutput.mp4" "$i"
 	    if [[ "$i" != *.mp4 ]]; then
 	        mv "$i" "${i%.*}.mp4"
-            fi
-	counter=$((counter - 1))
-    	fi
+		echo -ne "\e[0K\r${green}Transcoding: $(basename "$i"): CCOMPLETE${reset}"
+	      fi
+	else
+		echo -ne "\e[0K\r${red}Transcoding: $(basename "$i"): FAIL${reset}"
+		rm -r "$folderToParse/.tmpoutput.mp4"
+	fi
+    COUNTER=$((COUNTER - 1))
     echo ""
     echo ""
     fi
-    echo "Files left to process: $counter"
+    echo "Files left to process: $COUNTER"
 done
-
-CLEANUP
 
 echo "Happy streaming!"
--- move264.sh
+++ move264.sh
@@ -10,25 +10,43 @@ fi
 if [[ $1 != "" ]]; then
     folderToParse="$1"
 else
-    folderToParse="$PWD"
+    echo "Arg 1 needs to be the source folder"
+    exit 1
 fi
+
+if [[ $2 != "" ]]; then
+    COPYFOLDER="$2"
+else
+    echo "Arg 2 needs to be the source folder"
+    exit 1
+fi
+
+if [[ $3 != "" ]]; then
+    FOLDERMAX="$3"
+else
+    FOLDERMAX=$(du -s "$COPYFOLDER" | awk '{print $1}')
+fi
+
+
 echo -e "Looking for 264 files in: $folderToParse\n"
 
 SETSTATS () {
     FOLDERSIZE=$(du -s "$COPYFOLDER" | awk '{print $1}')
     FILESIZE=$(du -k "$fname" | cut -f 1)
+    FILENAME=$(basename "$fname")
     let SPACELEFT=FOLDERMAX-FOLDERSIZE
     #let SPACELEFTGB=SPACELEFT/1000000
 }
 
-FOLDERMAX=260000000
+#FOLDERMAX=$(du -s "/mnt/" | awk '{print $1}')
+#FOLDERMAX=260000000
 TOTAL=0
-COPYFOLDER="/RAID/265files/"
+#COPYFOLDER="/mnt/Daryl/264files/Movies/"
 IFS=$'\n'
 find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.m4v" | sort -n | while read fname; do
     SETSTATS "$fname"
-    echo -e "Free space left:\t$SPACELEFT GB"
-    if [[ $FILESIZE -lt $SPACELEFT ]]; then
+    echo -e "Free space left:\t$SPACELEFT KB"
+    if [[ $FILESIZE -lt $SPACELEFT ]] && [[ $(ls "$COPYFOLDER" | grep "$FILENAME" | wc -l) -eq 0 ]]; then
 	if [[ $(mediainfo "$fname" | grep "HEVC" | wc -l) -lt 1 ]]; then
 	    if [[ $(basename "$fname" | cut -c 1) == "." ]]; then
 		echo "I won't bother the transcoding going on in here :)"
@@ -37,6 +55,8 @@ find "$folderToParse" -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -ina
 		cp "$fname" "$COPYFOLDER"
 	    fi
 	fi
+    else
+	echo "File too big or already there!"
     fi
 done
 
